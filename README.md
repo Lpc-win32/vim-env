977d79f8de577275202d77accd46c6415bf1c11f
root <root@localhost.localdomain>
2017-01-22 17:50:27 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+> Author : Pengcheng Liu
+
+> make sure you floow softwares had been install:
+
+```
+git
+ctags
+```
+
+install :
+
+	./install.sh
---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -x
+
+mkdir -p ~/.vim/bundle
+
+cp -r molokai/colors ~/.vim
+
+if [ -f ~/.vimrc ] then
+	mv ~/.vimrc ~/.vimrc_
+fi
+
+cp ./.vimrc ~
+
+echo 'next step will expend long time, please wait'
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim -c 'PluginInstall'
+
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer
+
+echo 'vim-env is ok, good luck!'
